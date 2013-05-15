15717875a04aae6d5e2b86c6449489689cf6c456
fabienbk <fabien.benoit@gmail.com>
2013-05-15 22:23:04 +0200

    0.4.0 new files

---
+++ src/main/java/com/scramcode/djinn/ui/panels/DependencyDetailsPanelController.java
@@ -0,0 +1,65 @@
+package com.scramcode.djinn.ui.panels;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.swing.JTable;
+
+import com.scramcode.djinn.db.data.JavaItem;
+import com.scramcode.djinn.db.logic.ReferenceTools;
+import com.scramcode.djinn.model.GraphGranularityComboBoxModel.GranularityLevel;
+import com.scramcode.djinn.model.workspace.AbstractJavaItemTreeNode;
+import com.scramcode.djinn.ui.Application;
+import com.scramcode.djinn.ui.table.JavaItemTableModel;
+import com.scramcode.djinn.util.AbstractSwingWorker;
+
+public class DependencyDetailsPanelController {
+
+	private Application application;
+	private DependencyDetailsPanel dependencyDetailsPanel;
+
+
+	public DependencyDetailsPanelController(Application application) {
+		this.application = application;
+		this.dependencyDetailsPanel = application.getApplicationFrame().getDependencyDetailsPanel();		
+	}
+	
+	public void updateSourceSelection() {		
+		List<AbstractJavaItemTreeNode> selectionNode = application.getWorkspaceTreeController().getSelectionNode();
+		if (selectionNode.size() > 0) {
+			updateSourceSelection(selectionNode.get(0));
+		}
+	}
+	
+	public void updateSourceSelection(final AbstractJavaItemTreeNode sourceNode) {				
+		// Get the current granularity
+		final GranularityLevel granularityLevel = dependencyDetailsPanel.getGranularity();
+    		
+		// Get the current selected 'target' vertex in the graph
+		            
+		AbstractSwingWorker  worker = new AbstractSwingWorker() {
+		    @Override
+		    public Object construct() {
+		        return ReferenceTools.getAllReferences(sourceNode.getJavaItem(), granularityLevel);
+		    }
+		    
+		    @Override
+		    public void finished() {
+		        if (getValue() != null) {                        
+		        	Set<JavaItem> value = (Set<JavaItem>)getValue();							          
+		            JTable referencesTable = dependencyDetailsPanel.getReferencesTable();
+					
+		            referencesTable.clearSelection();
+		            JavaItemTableModel model = (JavaItemTableModel)referencesTable.getModel();
+		            model.setList(new ArrayList(value));
+		            		            
+//					JavaItemTableFactory.resizeTable(referencesTable);
+		        }                    
+		    }
+		    
+		};      
+		worker.start();
+	}
+	
+}
---
+++ src/main/java/com/scramcode/djinn/ui/panels/TabbedGraphAreaController.java
@@ -0,0 +1,68 @@
+/*
+ * Created on Feb 8, 2006
+ * Created by Fabien Benoit
+ * 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.scramcode.djinn.ui.panels;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import com.scramcode.djinn.db.data.JavaDependency;
+import com.scramcode.djinn.db.data.JavaItem;
+import com.scramcode.djinn.ui.Application;
+
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.graph.Graph;
+
+public class TabbedGraphAreaController {
+
+    private JTabbedPane graphAreaTabbedPane;
+    private Map<String, JPanel> tabNames = new HashMap<String, JPanel>();
+    
+    public TabbedGraphAreaController(Application application) {
+        this.graphAreaTabbedPane = application.getApplicationFrame().getTabbedPane();
+    }
+    
+    public void showGraph(String title, Graph<JavaItem, JavaDependency>  graphInfo) {
+        GraphPanelController graphController = new GraphPanelController(graphInfo);
+        addTab(title, graphController.getPanel());     
+    }
+    
+    public void addTab(String title, JPanel panel) {                               
+        graphAreaTabbedPane.add(title, panel);
+        tabNames.put(title, panel);
+    }       
+ 
+    public void deleteTab(String title) {                               
+        graphAreaTabbedPane.remove(tabNames.get(title));        
+    }
+    
+    public Graph<JavaItem, JavaDependency> getActiveGraph() {
+    	GraphPanel dependencyGraphPanel = (GraphPanel)graphAreaTabbedPane.getSelectedComponent();
+    	if (dependencyGraphPanel != null) {
+    		return dependencyGraphPanel.getGraph();
+    	}
+    	return null;
+    }
+
+    public void changeLayout(Layout<JavaItem, JavaDependency> newLayout) {
+    	GraphPanel dependencyGraphPanel = (GraphPanel)graphAreaTabbedPane.getSelectedComponent();
+    	dependencyGraphPanel.getVisualizationViewer().setGraphLayout(newLayout);
+    }
+}
